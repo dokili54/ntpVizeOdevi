0cd80dbc9c7c0dbafdeb5386b846dea427e88969
dokili54 <ayazzdogukan@gmail.com>
2021-04-22 22:11:10 +0300

    Add project files.

---
+++ ntpVizeOdevi/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace ntpVizeOdevi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_SehirAdi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Durum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Maks = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Bolge = new System.Windows.Forms.TextBox();
+            this.combo_Sehir = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şehir Seçiniz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Şehir Adı:";
+            // 
+            // txt_SehirAdi
+            // 
+            this.txt_SehirAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_SehirAdi.Location = new System.Drawing.Point(153, 46);
+            this.txt_SehirAdi.Name = "txt_SehirAdi";
+            this.txt_SehirAdi.Size = new System.Drawing.Size(192, 26);
+            this.txt_SehirAdi.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(13, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Durum:";
+            // 
+            // txt_Durum
+            // 
+            this.txt_Durum.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_Durum.Location = new System.Drawing.Point(153, 81);
+            this.txt_Durum.Name = "txt_Durum";
+            this.txt_Durum.Size = new System.Drawing.Size(192, 26);
+            this.txt_Durum.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(13, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Maksimum:";
+            // 
+            // txt_Maks
+            // 
+            this.txt_Maks.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_Maks.Location = new System.Drawing.Point(153, 117);
+            this.txt_Maks.Name = "txt_Maks";
+            this.txt_Maks.Size = new System.Drawing.Size(192, 26);
+            this.txt_Maks.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(13, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Bölge:";
+            // 
+            // txt_Bolge
+            // 
+            this.txt_Bolge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_Bolge.Location = new System.Drawing.Point(153, 152);
+            this.txt_Bolge.Name = "txt_Bolge";
+            this.txt_Bolge.Size = new System.Drawing.Size(192, 26);
+            this.txt_Bolge.TabIndex = 1;
+            // 
+            // combo_Sehir
+            // 
+            this.combo_Sehir.FormattingEnabled = true;
+            this.combo_Sehir.Location = new System.Drawing.Point(153, 13);
+            this.combo_Sehir.Name = "combo_Sehir";
+            this.combo_Sehir.Size = new System.Drawing.Size(192, 24);
+            this.combo_Sehir.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(352, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 165);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "gunes.png");
+            this.ımageList1.Images.SetKeyName(1, "azcloudy1.png");
+            this.ımageList1.Images.SetKeyName(2, "cloudy.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(525, 195);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.combo_Sehir);
+            this.Controls.Add(this.txt_Bolge);
+            this.Controls.Add(this.txt_Maks);
+            this.Controls.Add(this.txt_Durum);
+            this.Controls.Add(this.txt_SehirAdi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Hava Durumu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_SehirAdi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Durum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Maks;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Bolge;
+        private System.Windows.Forms.ComboBox combo_Sehir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList ımageList1;
+    }
+}
+

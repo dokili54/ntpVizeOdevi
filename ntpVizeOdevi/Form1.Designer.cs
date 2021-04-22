87488d3a10101931987234d90f80d96bad835bcb
dokili54 <ayazzdogukan@gmail.com>
2021-04-22 22:13:41 +0300

    Form tasarımı yapıldı ve load işlemi başlatıldı.

--- ntpVizeOdevi/Form1.Designer.cs
+++ ntpVizeOdevi/Form1.Designer.cs
@@ -171,6 +171,7 @@ namespace ntpVizeOdevi
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Hava Durumu";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- ntpVizeOdevi/Form1.cs
+++ ntpVizeOdevi/Form1.cs
@@ -16,5 +16,15 @@ namespace ntpVizeOdevi
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            havaDurumu();
+        }
+
+        public void havaDurumu()
+        {
+
+        }
     }
 }
